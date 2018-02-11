0930830c15a982c94d5353f2b0aac9aac328f542
Hayk <hkazaryan.97@gmail.com>
2018-02-11 18:41:54 +0400

    added new project for get data from myCore, errorCode:401

---
+++ myCore/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+
+public class Main {
+   WebDriver driver;
+    public static void main(String[] args) {
+        System.getProperty("java.class.path");
+        System.out.println(System.getProperty("java.class.path"));
+
+    }
+}
